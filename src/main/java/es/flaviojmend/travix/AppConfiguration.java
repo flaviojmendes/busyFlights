259adc79001896d55c168edba46b897d80880277
Flávio Mendes <flaviojmendes@gmail.com>
2017-02-17 08:51:17 -0200

    first commit

---
+++ src/main/java/es/flaviojmend/travix/AppConfiguration.java
@@ -0,0 +1,16 @@
+package es.flaviojmend.travix;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@ComponentScan
+@EnableAutoConfiguration
+@PropertySource(Constants.Properties)
+@Configuration
+public class AppConfiguration {
+
+
+
+}
---
+++ src/main/java/es/flaviojmend/travix/Constants.java
@@ -0,0 +1,10 @@
+package es.flaviojmend.travix;
+
+public class Constants {
+
+    /**
+     * properties file
+     */
+    public final static String Properties = "classpath:application.properties";
+
+}
